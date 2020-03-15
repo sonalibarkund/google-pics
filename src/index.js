8b986bc1f4eb208a09ded2a8acd9e54626d49384
sonali-amura <sonibarkund@gmail.com>
2020-03-15 10:32:58 +0530

    public folder added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App';
+import SearchBar from './components/SearchBar';
+
+ReactDom.render(
+  <App />,
+  document.querySelector("#root")
+);
